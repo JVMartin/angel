5ee3ef501a8989b7f8dedd5542ffb89a1aa8be00
Jacob Martin <mindnumb@gmail.com>
2016-07-06 16:13:35 -0700

    DRY'ing up user model factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,31 +1,21 @@
 <?php
 
 $factory->defineAs(App\User::class, 'user', function (Faker\Generator $faker) {
-    return [
-	    'role' => 'user',
-	    'name' => $faker->name,
-	    'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
+	return [
+		'role' => 'user',
+		'name' => $faker->name,
+		'email' => $faker->safeEmail,
+		'password' => bcrypt(str_random(10)),
+		'remember_token' => str_random(10),
+	];
 });
 
-$factory->defineAs(App\User::class, 'admin', function (Faker\Generator $faker) {
-    return [
-	    'role' => 'admin',
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
+$factory->defineAs(App\User::class, 'admin', function (Faker\Generator $faker) use ($factory) {
+	$user = $factory->raw(App\User::class);
+	return array_merge($user, ['role' => 'admin']);
 });
 
-$factory->defineAs(App\User::class, 'superadmin', function (Faker\Generator $faker) {
-    return [
-	    'role' => 'superadmin',
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
+$factory->defineAs(App\User::class, 'superadmin', function (Faker\Generator $faker) use ($factory) {
+	$user = $factory->raw(App\User::class);
+	return array_merge($user, ['role' => 'superadmin']);
 });
