87b8551acc3ea1a8fc57625d5393e511c7759136
Jacob Martin <mindnumb@gmail.com>
2016-07-06 16:11:19 -0700

    Factories for users, admins, and superadmins

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('role')->default('user');
+	        $table->string('role')->default('user');
+	        $table->string('name');
+	        $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
