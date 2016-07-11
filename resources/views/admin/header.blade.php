94e03c56206f569f262630fa7765e8b5832a16a8
Jacob Martin <mindnumb@gmail.com>
2016-07-11 13:39:55 -0700

    Refactor header to comply to Foundation Sites structure

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -1,24 +1,9 @@
-<nav id="adminNav" class="top-bar" data-topbar role="navigation">
-	<ul class="title-area">
-		<li class="name">
-			<h1><a href="{{ url('admin') }}">Angel Admin</a></h1>
-		</li>
-		<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-		<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-	</ul>
-
-	<section class="top-bar-section">
-		<!-- Right Nav Section -->
-		<ul class="right">
-			<li>
-				<a href="{{ url('admin/logout') }}">
-					Sign Out
-				</a>
+<div id="adminNav" class="top-bar">
+	<div class="top-bar-left">
+		<ul class="dropdown menu" data-dropdown-menu>
+			<li class="menu-text">
+				Angel Admin
 			</li>
-		</ul>
-
-		<!-- Left Nav Section -->
-		<ul class="left">
 			<li{!! Request::is('admin/pages*') ? ' class="active"' : '' !!}>
 				<a href="{{ url('admin/pages') }}">
 					Pages
@@ -35,5 +20,14 @@
 				</a>
 			</li>
 		</ul>
-	</section>
-</nav>
+	</div>
+	<div class="top-bar-right">
+		<ul class="menu">
+			<li>
+				<a href="{{ url('admin/logout') }}">
+					Sign Out
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
