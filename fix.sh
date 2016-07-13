0aae9948359e18ad8ecbb33d12b09c4f4dc087ac
Jacob Martin <mindnumb@gmail.com>
2016-07-13 14:19:49 -0700

    Make the permissions fixing script a little more robust

--- fix.sh
+++ fix.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # Copyright (c) 2016 Jacob Martin
+# MIT license
+
 # Fixes the permissions of the application for secure serving.
 # Note that "www-data" is used here; feel free to change this for your own
 # web server's username.
 
+# If you don't have permissions to modify files and folders in the first place,
+# this script will not work for you until you fix that with:
+# sudo chown $USER -R .
+
 function allowWrite() {
-	cd $1
-	find . -type f -exec chmod 664 {} +
-	find . -type d -exec chmod 775 {} +
-	chown $USER:www-data -R .
-	cd ~-
+	find $1 -type f -exec chmod 664 {} +
+	find $1 -type d -exec chmod 775 {} +
+	chown $USER:www-data -R $1
 }
 
 find . -type f -exec chmod 644 {} +
