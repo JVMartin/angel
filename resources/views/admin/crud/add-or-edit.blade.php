dd00a3d1e720b9947760eabf8a0962c378085f6f
Jacob Martin <mindnumb@gmail.com>
2016-07-27 23:05:03 -0700

    Don't indent sections

--- resources/views/admin/crud/add-or-edit.blade.php
+++ resources/views/admin/crud/add-or-edit.blade.php
@@ -7,41 +7,41 @@
 @endsection
 
 @section('content')
-	<section id="crudAddOrEdit">
+<section id="crudAddOrEdit">
+	<div class="row">
+		<div class="columns small-12">
+			<p>
+				<a href="{{ $repository->getIndexURL() }}">
+					<i class="fa fa-arrow-left"></i>
+					Back to index
+				</a>
+			</p>
+			<h1>{{ ucfirst($action) . ' ' . $repository->getSingular() }}</h1>
+		</div>
+	</div>
+	@if ($action == 'edit')
+		{!! Form::model($model) !!}
+	@elseif ($action == 'add')
+		{!! Form::open() !!}
+	@endif
+		@include('admin.crud.cols', ['cols' => $repository->getCols()])
 		<div class="row">
 			<div class="columns small-12">
-				<p>
-					<a href="{{ $repository->getIndexURL() }}">
-						<i class="fa fa-arrow-left"></i>
-						Back to index
-					</a>
-				</p>
-				<h1>{{ ucfirst($action) . ' ' . $repository->getSingular() }}</h1>
+				<button type="submit" class="button small">
+					Save
+				</button>
 			</div>
 		</div>
-		@if ($action == 'edit')
-			{!! Form::model($model) !!}
-		@elseif ($action == 'add')
-			{!! Form::open() !!}
-		@endif
-			@include('admin.crud.cols', ['cols' => $repository->getCols()])
-			<div class="row">
-				<div class="columns small-12">
-					<button type="submit" class="button small">
-						Save
-					</button>
-				</div>
-			</div>
-		{!! Form::close() !!}
-		<div class="row">
-			<div class="columns small-12">
-				<p>
-					<a href="{{ $repository->getIndexURL() }}">
-						<i class="fa fa-arrow-left"></i>
-						Back to index
-					</a>
-				</p>
-			</div>
+	{!! Form::close() !!}
+	<div class="row">
+		<div class="columns small-12">
+			<p>
+				<a href="{{ $repository->getIndexURL() }}">
+					<i class="fa fa-arrow-left"></i>
+					Back to index
+				</a>
+			</p>
 		</div>
-	</section>
+	</div>
+</section>
 @endsection
--- resources/views/admin/crud/index.blade.php
+++ resources/views/admin/crud/index.blade.php
@@ -5,69 +5,69 @@
 @endsection
 
 @section('content')
-	<section id="crudIndex">
-		<div class="row">
-			<div class="small-6 column">
-				<h1>
-					{{ $repository->getPlural() }}
-				</h1>
-			</div>
-			<div class="small-6 column">
-				<p class="text-right">
-					<a href="{{ $repository->getAddURL() }}" class="button large">
-						<i class="fa fa-plus"></i>
-						Add {{ $repository->getSingular() }}
-					</a>
-				</p>
-			</div>
+<section id="crudIndex">
+	<div class="row">
+		<div class="small-6 column">
+			<h1>
+				{{ $repository->getPlural() }}
+			</h1>
 		</div>
-		@include('admin.crud.index-search')
-		<div class="pagination-centered">
-			{{-- {!! $models->render(Foundation::paginate($models)) !!} --}}
+		<div class="small-6 column">
+			<p class="text-right">
+				<a href="{{ $repository->getAddURL() }}" class="button large">
+					<i class="fa fa-plus"></i>
+					Add {{ $repository->getSingular() }}
+				</a>
+			</p>
 		</div>
-		<div class="row">
-			<div class="small-12 column">
-				<table>
-					<thead>
-						<tr>
-							<th></th>
-							@foreach ($repository->getIndexCols() as $col)
-								<th>
-									<a href="{{ url('admin/' . $repository->getHandle() . '/order-by/' . $col) }}">
-										{!! $repository->getCols()[$col]['pretty'] !!}
-										@if (session('admin.' . $repository->getHandle() . '.order.column') == $col)
-											@if (session('admin.' . $repository->getHandle() . '.order.direction') == 'ASC')
-												<i class="fa fa-chevron-up"></i>
-											@else
-												<i class="fa fa-chevron-down"></i>
-											@endif
+	</div>
+	@include('admin.crud.index-search')
+	<div class="pagination-centered">
+		{{-- {!! $models->render(Foundation::paginate($models)) !!} --}}
+	</div>
+	<div class="row">
+		<div class="small-12 column">
+			<table>
+				<thead>
+					<tr>
+						<th></th>
+						@foreach ($repository->getIndexCols() as $col)
+							<th>
+								<a href="{{ url('admin/' . $repository->getHandle() . '/order-by/' . $col) }}">
+									{!! $repository->getCols()[$col]['pretty'] !!}
+									@if (session('admin.' . $repository->getHandle() . '.order.column') == $col)
+										@if (session('admin.' . $repository->getHandle() . '.order.direction') == 'ASC')
+											<i class="fa fa-chevron-up"></i>
+										@else
+											<i class="fa fa-chevron-down"></i>
 										@endif
-									</a>
-								</th>
+									@endif
+								</a>
+							</th>
+						@endforeach
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($models as $model)
+						<tr>
+							<td>
+								<a href="{{ url($model->editURL()) }}" class="button tiny editButton">
+									<i class="fa fa-edit"></i>
+								</a>
+							</td>
+							@foreach ($repository->getIndexCols() as $col)
+								<td>
+									{{ $model->$col }}
+								</td>
 							@endforeach
 						</tr>
-					</thead>
-					<tbody>
-						@foreach ($models as $model)
-							<tr>
-								<td>
-									<a href="{{ url($model->editURL()) }}" class="button tiny editButton">
-										<i class="fa fa-edit"></i>
-									</a>
-								</td>
-								@foreach ($repository->getIndexCols() as $col)
-									<td>
-										{{ $model->$col }}
-									</td>
-								@endforeach
-							</tr>
-						@endforeach
-					</tbody>
-				</table>
-			</div>
+					@endforeach
+				</tbody>
+			</table>
 		</div>
-		<div class="pagination-centered">
-			{{-- {!! $models->render(Foundation::paginate($models)) !!} --}}
-		</div>
-	</section>
+	</div>
+	<div class="pagination-centered">
+		{{-- {!! $models->render(Foundation::paginate($models)) !!} --}}
+	</div>
+</section>
 @endsection
