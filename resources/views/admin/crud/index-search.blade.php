aea8a3e0a9f9ba67261f62912c30e86beb05d3c3
Jacob Martin <mindnumb@gmail.com>
2016-07-29 21:04:50 -0700

    Fix search bar for crud indexes

--- resources/views/admin/crud/index-search.blade.php
+++ resources/views/admin/crud/index-search.blade.php
@@ -1,16 +1,12 @@
 <div class="row">
 	<div class="small-12 medium-6 columns">
 		{!! Form::open(['url' => '/admin/' . $repository->getHandle() . '/search']) !!}
-		<div class="row collapse">
-			<div class="small-10 columns">
-				{!! Form::text('search', session('admin.' . $repository->getHandle() . '.search'), ['autofocus']) !!}
+			<div class="input-group">
+				{!! Form::text('search', session('admin.' . $repository->getHandle() . '.search'), ['autofocus', 'class' => 'input-group-field']) !!}
+				<div class="input-group-button">
+					<input type="submit" class="button" value="Search" />
+				</div>
 			</div>
-			<div class="small-2 columns">
-				<button class="button postfix">
-					Search
-				</button>
-			</div>
-		</div>
 		{!! Form::close() !!}
 	</div>
 </div>
