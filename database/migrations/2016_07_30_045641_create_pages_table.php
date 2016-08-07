0e6cd53bb7b88c6832a1828e1a8d0923865670d0
Jacob Martin <mindnumb@gmail.com>
2016-08-06 23:18:01 -0700

    Fix whitespace

--- database/migrations/2016_07_30_045641_create_pages_table.php
+++ database/migrations/2016_07_30_045641_create_pages_table.php
@@ -5,40 +5,40 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePagesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-	    Schema::create('pages', function (Blueprint $table) {
-		    $table->increments('id');
-		    $table->string('slug')->unique();
-		    $table->string('title');
-		    $table->string('image');
-		    $table->string('og_desc', 300);
-		    $table->text('html');
-		    $table->text('plaintext');
-		    $table->timestamps();
-	    });
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('pages', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('slug')->unique();
+			$table->string('title');
+			$table->string('image');
+			$table->string('og_desc', 300);
+			$table->text('html');
+			$table->text('plaintext');
+			$table->timestamps();
+		});
 
-	    DB::statement('
+		DB::statement('
 			ALTER TABLE `pages` ADD FULLTEXT search(
 				`title`,
 				`plaintext`
 		)');
-    }
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-	    DB::statement('SET foreign_key_checks = 0');
-	    Schema::drop('pages');
-	    DB::statement('SET foreign_key_checks = 1');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::statement('SET foreign_key_checks = 0');
+		Schema::drop('pages');
+		DB::statement('SET foreign_key_checks = 1');
+	}
 }
