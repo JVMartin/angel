daa3dd2991428e8373285d23bbe6fec7af5d8c3f
Jacob Martin <mindnumb@gmail.com>
2017-05-10 20:30:56 -0700

    Update admin routes

--- routes/web-admin.php
+++ routes/web-admin.php
@@ -1,33 +1,25 @@
 <?php
-/**
- * @copyright (c) 2016 Jacob Martin
- * @license MIT https://opensource.org/licenses/MIT
- */
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
-	Route::get('/', 'LoginController@dashboardOrSignIn');
-	Route::post('/sign-in', 'LoginController@login');
-
-	//---------------------
-	// Crud Controllers
-	//---------------------
-	if ( ! function_exists('crudController')) {
-		function crudController($controller) {
-			return function() use ($controller) {
-				Route::get('/', $controller . '@getIndex');
-				Route::post('search', $controller . '@postSearch');
-				Route::get('order-by/{column}', $controller . '@getOrderBy');
-				Route::get('add', $controller . '@getAdd');
-				Route::post('add', $controller . '@postAdd');
-				Route::get('edit/{id}', $controller . '@getEdit');
-				Route::post('edit/{id}', $controller . '@postEdit');
-			};
-		}
-	}
-	Route::group(['middleware' => 'admin', 'namespace' => 'Crud'], function() {
-		Route::group(['prefix' => 'users'], crudController('UserController'));
-		Route::group(['prefix' => 'pages'], crudController('PageController'));
-
-		Route::get('changes/{crudRepository}/{id}/{column}', 'ChangesController@getLog');
+	Route::get('/', function() {
+		return redirect()->route('admin.users.index');
 	});
+
+    Route::group(['middleware' => 'admin', 'namespace' => 'Crud'], function() {
+        // Users
+        Route::group(['prefix' => 'users'], function() {
+            Route::get('/', 'UserController@getIndex')->name('admin.users.index');
+            Route::post('search', 'UserController@postSearch')->name('admin.users.index.search');
+            Route::get('order-by/{column}', 'UserController@getOrderBy')->name('admin.users.index.order-by');
+            Route::get('add', 'UserController@getAdd')->name('admin.users.add');
+            Route::post('add', 'UserController@postAdd');
+            Route::get('show/{hashid}', 'UserController@getShow')->name('admin.users.show');
+            Route::get('edit/{hashid}', 'UserController@getEdit')->name('admin.users.edit');
+            Route::post('edit/{hashid}', 'UserController@postEdit');
+            Route::post('edit/{hashid}/password', 'UserController@postPassword')->name('admin.users.edit.password');
+            Route::delete('{hashid}', 'UserController@delete')->name('admin.users.delete');
+        });
+
+        Route::get('changes/{crudRepository}/{hashid}/{column}', 'ChangesController@getLog')->name('admin.changes.log');
+    });
 });
