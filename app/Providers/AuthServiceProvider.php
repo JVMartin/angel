e4208d22e660ba931f7ee87e5eecb913264a0b08
Jacob Martin <mindnumb@gmail.com>
2017-05-10 20:21:27 -0700

    Update auth provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,12 +1,8 @@
 <?php
-/**
- * @copyright (c) 2016 Jacob Martin
- * @license MIT https://opensource.org/licenses/MIT
- */
 
 namespace App\Providers;
 
-use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,30 +12,27 @@ class AuthServiceProvider extends ServiceProvider
 	 *
 	 * @var array
 	 */
-	/*
 	protected $policies = [
 		'App\Model' => 'App\Policies\ModelPolicy',
 	];
-	*/
 
 	/**
-	 * Register any application authentication / authorization services.
+	 * Register any authentication / authorization services.
 	 *
-	 * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
 	 * @return void
 	 */
-	public function boot(GateContract $gate)
+	public function boot()
 	{
-		$this->registerPolicies($gate);
+		$this->registerPolicies();
 
 		// Let super administrators do anything.
-		$gate->before(function ($user, $ability) {
+		Gate::before(function ($user, $ability) {
 			if ($user->isSuperAdmin()) {
 				return true;
 			}
 		});
-		
-		$gate->define('admin', function ($user) {
+
+		Gate::define('admin', function ($user) {
 			return $user->isAdmin();
 		});
 	}
