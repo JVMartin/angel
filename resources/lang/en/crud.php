e789ee40028032863642bf53bfc0a1f29ced7508
Jacob Martin <mindnumb@gmail.com>
2017-05-10 21:17:44 -0700

    CRUD language lines

---
+++ resources/lang/en/crud.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | CRUD Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during CRUD actions in the
+    | administrative panel.
+    |
+    */
+
+    'created' => ':type created.',
+    'updated' => ':type updated.',
+    'deleted' => ':type deleted.',
+
+];
