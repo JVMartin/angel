3c76d1114e26052caac85031d2396be73e3599c9
Jacob Martin <mindnumb@gmail.com>
2017-05-10 21:06:11 -0700

    Configure lfm

--- config/lfm.php
+++ config/lfm.php
@@ -44,8 +44,8 @@ return [
     // You should create routes to serve images if it is not set to public.
     'base_directory' => 'public',
 
-    'images_folder_name' => 'photos',
-    'files_folder_name'  => 'files',
+    'images_folder_name' => 'uploads/images',
+    'files_folder_name'  => 'uploads/files',
 
     'shared_folder_name' => 'shares',
     'thumb_folder_name'  => 'thumbs',
