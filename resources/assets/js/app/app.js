abde6c44a524cb355a493975ce9e0110e1588c6c
Jacob Martin <mindnumb@gmail.com>
2017-05-11 18:18:44 -0700

    More bootstrappin' for the App side of things

--- resources/assets/js/app/app.js
+++ resources/assets/js/app/app.js
@@ -1,1 +1,3 @@
+import './bootstrap';
+
 $(document).foundation();
---
+++ resources/assets/js/app/bootstrap.js
@@ -0,0 +1,6 @@
+// Window-bound objects.
+window._ = require('lodash');
+window.$ = window.jQuery = require('jquery');
+
+// External packages - order matters.
+require('foundation-sites');
